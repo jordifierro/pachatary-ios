011c170b1a7b369537bfba8a7df463f3cc484e74
jordifierro <jordifierromulero@gmail.com>
2018-10-16 18:03:15 +0200

    Remove unused duplicated code

--- Pachatary/Presentation/Profile/ProfileDependencyInjector.swift
+++ Pachatary/Presentation/Profile/ProfileDependencyInjector.swift
@@ -4,11 +4,6 @@ import Moya
 
 class ProfileDependencyInjector {
     
-    static func exploreExperiencePresenter(view: ExploreExperiencesView) -> ExploreExperiencesPresenter {
-        return ExploreExperiencesPresenter(ExperienceDataDependencyInjector.experienceRepository,
-                                           AppPresentationDependencyInjector.mainScheduler, view)
-    }
-    
     static func profilePresenter(view: ProfileView, username: String) -> ProfilePresenter {
         return ProfilePresenter(ExperienceDataDependencyInjector.experienceRepository,
                                 ProfileDataDependencyInjector.profileRepository,
